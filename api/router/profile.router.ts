70a2fff94258bbfb93ba104c3578016a3a6e13fb
ZeynalliZeynal <zzeynalli446@gmail.com>
2025-03-30 18:12:27 +0400

    fix: remove userAgent middleware from profile controller

--- api/router/profile.router.ts
+++ api/router/profile.router.ts
@@ -1,11 +1,10 @@
-import { getUserAgent } from "./../middleware/user-agent.middleware";
 import express from "express";
 import { getProfile, updateProfile } from "../controller/profile.controller";
 import { authenticate } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
-router.get("/", authenticate, getUserAgent, getProfile);
+router.get("/", authenticate, getProfile);
 router.patch("/update", authenticate, updateProfile);
 
 export default router;
