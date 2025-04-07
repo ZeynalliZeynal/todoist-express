e8b8e0a71c89733fb343ea386b23ae6809371e84
Zeynalli Zeynal <zzeynalli446@gmail.com>
2025-04-07 10:04:52 +0400

    commit

--- dist/constants/bucket-prefixes.js
+++ dist/constants/bucket-prefixes.js
@@ -4,6 +4,7 @@ exports.isValidBucketPrefix = exports.BUCKET_PREFIXES = void 0;
 var BUCKET_PREFIXES;
 (function (BUCKET_PREFIXES) {
     BUCKET_PREFIXES["AVATARS"] = "avatars";
+    BUCKET_PREFIXES["FEEDBACKS"] = "feedbacks";
 })(BUCKET_PREFIXES || (exports.BUCKET_PREFIXES = BUCKET_PREFIXES = {}));
 const isValidBucketPrefix = (value) => Object.values(BUCKET_PREFIXES).includes(value);
 exports.isValidBucketPrefix = isValidBucketPrefix;
